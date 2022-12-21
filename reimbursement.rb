7f407f776359912b1397f624dbd8027784fff307
Steven Dawn <stevendawn98@gmail.com>
2022-12-21 17:26:29 -0500

    calculated full days in project

--- reimbursement.rb
+++ reimbursement.rb
@@ -2,4 +2,8 @@ def calculate_reimbursement(projects)
     reimbursement = 0
 
     projects = projects.sort_by { |p| p[:start_date]}
+
+    projects.each_with_index do |project, index|
+        full_days = (project[:end_date] - project[:start_date] + 1) / 2
+    end
 end
